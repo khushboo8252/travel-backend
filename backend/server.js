8a07abbe0bdd0f518c3a62d484a67496138e9855
khushboo8252 <khushbooranjan8252@gmail.com>
2024-12-22 12:09:54 +0530

    some change

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Define routes
+// app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/trips', require('./routes/tripRoutes'));
 app.use('/api/bookings', require('./routes/bookingRoutes')); // Add booking routes
